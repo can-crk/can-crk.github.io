a54036c66b6485d4c3e5ffcce6268175ac68a25e
can-crk <102350029+can-crk@users.noreply.github.com>
2023-11-03 19:43:10 +0000

    code/folder cleanup

--- git-cv.js
+++ git-cv.js
@@ -1,48 +1,50 @@
 const graphContainer = document.getElementById("graph-container");
 
-    const options = {
-      template: GitgraphJS.templateExtend("metro", {
-        colors: ["#02394A", "#598ABA", "#5158BB", "#F26DF9", "#EB4B98"],
-        commit: {
-          message: {
-            displayAuthor: false,
-            displayHash: false
-          }
-        }
-      })
-    };
-    const gitgraph = GitgraphJS.createGitgraph(graphContainer, options);
+const options = {
+  template: GitgraphJS.templateExtend("metro", {
+    colors: ["#02394A", "#598ABA", "#5158BB", "#F26DF9", "#EB4B98"],
+    commit: {
+      message: {
+        displayAuthor: false,
+        displayHash: false,
+      },
+    },
+  }),
+};
+const gitgraph = GitgraphJS.createGitgraph(graphContainer, options);
 
-    const master = gitgraph.branch("master");
+const master = gitgraph.branch("master");
 
-    master.commit("2000 | Initial commit");
+master.commit("2000 | Initial commit");
 
-    const schulischeBildung = master.branch("Schulische Bildung");
-    schulischeBildung.commit("Realschulabschluss an der Realschule Jöllenbeck");
-    schulischeBildung.commit("Abitur an der Martin-Niemöller Gesamtschule");
-    master.merge(schulischeBildung, "2021");
+const schulischeBildung = master.branch("Schulische Bildung");
+schulischeBildung.commit("Realschulabschluss an der Realschule Jöllenbeck");
+schulischeBildung.commit("Abitur an der Martin-Niemöller Gesamtschule");
+master.merge(schulischeBildung, "2021");
 
-    const beruf = master.branch("Beruf");
-    
-    beruf.commit("Start der Ausbildung zum Fachinformatiker für Anwendungsentwicklung bei Schüco");
+const beruf = master.branch("Beruf");
 
-    master.commit("2022");
+beruf.commit(
+  "Start der Ausbildung zum Fachinformatiker für Anwendungsentwicklung bei Schüco"
+);
 
-    beruf.commit("Eintritt in die JAV")
+master.commit("2022");
 
-    master.commit("2023");
-    const nebenberuflichKleinunternehmer = master.branch("Can Carikcioglu Development");
-    const hackerSchool = beruf.branch("Hacker School");
+beruf.commit("Eintritt in die JAV");
 
+master.commit("2023");
+const nebenberuflichKleinunternehmer = master.branch(
+  "Can Carikcioglu Development"
+);
+const hackerSchool = beruf.branch("Hacker School");
 
-    hackerSchool.commit("Erster Kurs als Inspirer");
+hackerSchool.commit("Erster Kurs als Inspirer");
 
+nebenberuflichKleinunternehmer.commit("Anmeldung und erste Projekte");
 
-    nebenberuflichKleinunternehmer.commit("Anmeldung und erste Projekte");
+master.commit("heute");
 
-    master.commit("heute");
+// beruf.commit("");
 
-    // beruf.commit("");
-
-    // beruf.merge(schulischeBildung2);
-    // master.merge(beruf);
+// beruf.merge(schulischeBildung2);
+// master.merge(beruf);
